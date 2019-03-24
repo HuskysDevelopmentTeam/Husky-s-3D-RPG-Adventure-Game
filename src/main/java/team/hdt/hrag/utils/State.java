fe699217f326b0b0d60ebf32895d10759735ff88
OliviaTheVampire <sindrefagerheim53@gmail.com>
2019-03-24 07:53:43 +0100

    last progress from laptop

---
+++ src/main/java/team/hdt/hrag/utils/State.java
@@ -0,0 +1,15 @@
+package team.hdt.hrag.utils;
+
+public class State {
+
+    public int scancode, key;
+    public boolean pressed, down, released;
+
+    public State(int key) {
+
+        this.key = key;
+
+    }
+
+
+}
